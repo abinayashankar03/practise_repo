20f2165d875b9d00763707d08bf6f49402d63381
abinayashankar03 <s.abinayabba2021@gmail.com>
2026-09-26 18:43:24 +0530

    stash solved

--- one.js
+++ one.js
@@ -1,3 +1,4 @@
 console.log("welcome to zenv");
 console.log("github is an online platform to perform git");
 console.log("git is a version control software");
+console.log("git helps to syncronize the code");
